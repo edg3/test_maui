0856cd1ae0ba476b4145a6042d5a7bba031c662f
edg3 <loveland.ernest@gmail.com>
2022-06-14 11:42:11 +0200

    import sample

---
+++ maui_3/maui_3/TestViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maui_3
+{
+    public class TestViewModel
+    {
+        string TestName { get; set; } = "Nobody";
+    }
+}
---
+++ maui_3/maui_3/TestViewModelLocator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maui_3
+{
+    public class TestViewModelLocator
+    {
+        public TestViewModel TestVM { get; init; } = new TestViewModel();
+    }
+}
